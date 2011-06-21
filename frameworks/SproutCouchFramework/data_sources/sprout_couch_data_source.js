c2df56989ccbe86e6ac999e7bc0a69c508a9ea7f
Cortland Klein <me@pixelcort.com>
2011-06-20 20:57:18 -0700

    Experimental #fetch method supporting both local and remote queries.

--- frameworks/SproutCouchFramework/data_sources/sprout_couch_data_source.js
+++ frameworks/SproutCouchFramework/data_sources/sprout_couch_data_source.js
@@ -3,6 +3,41 @@
 SproutCouch.DataSource = SC.DataSource.extend({
   database: '',
 
+  fetch: function(store, query) {
+    // Note, these properties are temporary and are going to change!
+    if (!query.temprary_url_fragment) throw SC.$error('temporary_url_fragment was not provided!');
+    if (!query.temporary_url_method) throw  SC.$error('temporary_url_method was not provided!');
+    var needs_body = ['PUT','POST'].indexOf(query.temporary_url_method)>-1;
+    if (needs_body && !query.temporary_url_body) throw SC.$error('temporary_url_body was not provided!');
+    if (!query.respondsTo('sproutCouchDataSourceDidComplete')) throw SC.$error('sproutCouchDataSourceDidComplete callback was not provided!');
+
+    var req = SC.Request.create()
+                        .set('address', '/'+this.database+'/'+query.temporary_url_fragment)
+                        .set('type', query.temporary_url_method)
+                        .notify(this, 'fetchDidComplete', {
+                          store: store,
+                          query: query
+                        })
+                        .send(query.temporary_url_body); // Will be undefined for GET and DELETE, but that's okay. :)
+    return YES;
+  },
+  fetchDidComplete: function(request, params) {
+    var response = request.get('response');
+    var callbackResult = params.query.sproutCouchDataSourceDidComplete({
+      temporary_store: params.store
+    });
+    if (callbackResult.error) {
+      params.store.dataSourceDidErrorQuery(params.query, callbackResult.error);
+    } else if (callbackResult.remoteStoreKeys || params.query.isRemote) {
+      if (!callbackResult.remoteStoreKeys) throw SC.$error('remote queries need remoteStoreKeys!');
+      if (!params.query.isRemote)          throw SC.$error('local queries don\'t need remoteStoreKeys!');
+      params.store.loadQueryResults(params.store, callbackResult.remoteStoreKeys);
+      // loadQueryResults calls dataSourceDidFetchQuery for us. :)
+    } else {
+      params.store.dataSourceDidFetchQuery(params.query);
+    }
+  },
+
   retrieveRecords: function(store, storeKeys, ids) {
     if (storeKeys.length !== ids.length) {
       throw SC.$error('storeKeys and ids were not the same length!');
@@ -21,6 +56,7 @@ SproutCouch.DataSource = SC.DataSource.extend({
     var response = request.get('response'),
         dataHashes = response.rows.map(function(row){return row.doc;});
     if (dataHashes.length !== params.storeKeys.length) throw SC.$error('lengths did not match');
+    // Note: This appears to call dataSourceDidComplete, not pushRetreive; this might be useful for when we add error handling
     params.store.loadRecords(params.storeKeys.map(function(storeKey){return params.store.recordTypeFor(storeKey);}), dataHashes);
   },
 
